0258c6edf656412ed301852438d715d8a3cad197
Shaan3110 <hitarasigan0987654321@gmail.com>
2021-12-21 11:41:10 +0530

    connected mongodb atlas to server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const mongoose=require('mongoose');
+
+
+//database address
+const dbaddress='mongodb+srv://mediocareteam:mediocareteam@cluster0.6dm6o.mongodb.net/Account?retryWrites=true&w=majority';
+
+//connection
+mongoose.connect(dbaddress).then(()=>{
+    console.log("connection successful");
+}).catch((err)=>{
+    console.log(err);
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
