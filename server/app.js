bfa94b67c1df948e227ee31370c3ef41786659cc
Shaan3110 <hitarasigan0987654321@gmail.com>
2021-12-23 16:05:20 +0530

    added file format as jsx

--- server/app.js
+++ server/app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const connectaccount = require('./databases/Account');
 const connectrequest = require('./databases/Requestbed');
-const app = express()
+const app = express();
+
+//port
 const port = 3001
 
 // connectaccount();
 connectrequest();
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+//Routes
+app.use('/auth/',require('./routes/Account.js'))
+
+
+//server listening 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   })
