91260f3eec5f4c180f1fc0de162c3d5644642f99
Shaan3110 <hitarasigan0987654321@gmail.com>
2021-12-24 18:00:28 +0530

    debugging

--- server/app.js
+++ server/app.js
@@ -17,6 +17,6 @@ app.get('/', (req, res) => {
 
 
 //server listening 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
