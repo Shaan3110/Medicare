304804fa0ca8debc0bb19cbc5c5eaff374cb9438
Shaan3110 <hitarasigan0987654321@gmail.com>
2021-12-31 21:25:14 +0530

    approval of beds

--- server/routes/Bed.js
+++ server/routes/Bed.js
@@ -3,7 +3,6 @@ const express = require('express');
 //all module imports
 const Bed = require('../models/Bed');
 
-
 //middleware for token
 const Userdata= require('../middleware/Userdata');
 
@@ -11,20 +10,21 @@ const Userdata= require('../middleware/Userdata');
 const router = express.Router();
 
 
-
-
-//all beds showing route
-router.get('/user/showtickets',
+//all beds showing route which are approved by the hospital for the user and same on the hospital side more as a history
+router.get('/showtickets/approved',
     Userdata,
     async (req, res) => {
         try {
 
             //find command returns all the rows of the database
             let ticket;
+
+            //checking if req has hospital or user as the object based on that the search is being made. The user or hospital should be able to
+            //see their corresponding tickets only and the token is dcrypted and used that data for showing the result
             if(req.hospital)
             {
                 ticket=await Bed.find({hospital:req.hospital.id});
-                res.send(ticket);
+                res.json(ticket);
             }
             if(req.user)
             {
@@ -53,13 +53,18 @@ router.get('/user/showtickets',
 router.post('/addticket/:hid',
     Userdata,
     async (req, res) => {
+
+
         try {
 
 
             //creating a new record with user id , hospital id and approve
             //another way to create a data other than the create function used in Account
             const bed=new Bed({
+
+                //the user is sent through the auth-token which is fetched from req which was added using the middleware
                 user:req.user.id,
+                //the hospital is in the req params and sent from the client on the url itself to be fetched
                 hospital:req.params.hid,
                 approve:false,
                 //date is save in unix timestamp so that it can be changed to local time and showed how many days earlier the request is being
@@ -82,5 +87,129 @@ router.post('/addticket/:hid',
             });
         }
     });
+
+
+
+
+
+//verifying if the token if of user or hospital
+router.post('/checktoken',
+    Userdata,
+    async (req, res) => {
+        try {
+
+
+            //as userdata puts the user or hospital id on the req so we are using that to check if it is user or hospital
+            if(req.user)
+            {
+                res.status(200).send("User");
+            }
+            else if(req.hospital)
+            {
+                res.status(200).send("Hospital");
+            }
+            //the error is sent from Userdata module in case of wrong token but if any other problem occurs an error would be throwed to the 
+            //server
+            else
+            {
+                throw "error";
+            }
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).json({
+                "errors": [{
+                "value": "no-value",
+                "msg": "Sorry for the inconvinience some internal server error occurred",
+                "param": "no-param",
+                "location": "server"
+                }]
+            });
+        }
+    });
+
+
+
+
+
+//approve a bed request for the hospital
+router.post('/approve/:bid',
+    Userdata,
+    async (req, res) => {
+        try {
+            if(req.hospital)
+            {
+                const {action}=req.body;
+
+                //checking if the bed really exists and has been requested from the concerned hospital only to avoid hackers
+                let bed=await Bed.findById(req.params.bid);
+
+                //first check is if the bed exists on the database
+                if(!bed)
+                {
+                        return res.status(404).json({
+                            "errors": [{
+                            "value": "no-value",
+                            "msg": "Requested bed doesn't exist",
+                            "param": "no-param",
+                            "location": "server"
+                            }]
+                        });
+                }
+
+                //second check is if the bed approval or denial request is from the authorized hospital
+                if(bed.hospital.toString()!==req.hospital.id)
+                {
+                        return res.status(401).json({
+                            "errors": [{
+                            "value": "no-value",
+                            "msg": "Your session has expired",
+                            "param": "no-param",
+                            "location": "server"
+                            }]
+                        });
+                }
+
+                //if action is true sent in the body of the req then approve the bed request 
+                if(action===true)
+                {
+                    const updatedBed={};
+                    //adding a element to the json as approve and marking it as true
+                    updatedBed.approve=true;
+                    bed=await Bed.findByIdAndUpdate(req.params.bid,{$set:updatedBed},{new:true});
+                    res.status(200).send(bed);
+                }
+
+                
+                //else if the actio is false then delete the bed request
+                else if(action===false)
+                {
+                    bed=await Bed.findByIdAndDelete()
+                    res.status(200).send(bed);
+                }
+            }
+            else
+            {
+                return res.status(401).json({
+                    "errors": [{
+                    "value": "no-value",
+                    "msg": "Your session has expired",
+                    "param": "no-param",
+                    "location": "server"
+                    }]
+                });
+            }
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).json({
+                "errors": [{
+                "value": "no-value",
+                "msg": "Sorry for the inconvinience some internal server error occurred",
+                "param": "no-param",
+                "location": "server"
+                }]
+            });
+        }
+    })
+
 
 module.exports = router;
